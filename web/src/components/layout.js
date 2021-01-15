51ca7c7af5fde49cd1026e0eec982f0c2a8bd1f6
Philipp Veller <mail@vellip.de>
2021-01-15 16:43:58 +0100

    * add basic styling * rewrite layout component   * put header and footer into layout.js since it is not needed anywhere else   * simplify header and adapt to old portfolio

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -1,22 +1,97 @@
 import React from 'react'
-import Header from './header'
+import hStyles from './header.module.scss'
+import fStyles from './footer.module.scss'
+import styles from './layout.module.scss'
+import {Link} from 'gatsby'
+import {cn, rnItem} from '../lib/helpers'
+import Container from './container'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGithub, faLinkedinIn, faMediumM, faTwitter} from '@fortawesome/free-brands-svg-icons'
 
-import '../styles/layout.css'
-import styles from './layout.module.css'
+const links = [
+  {label: 'Projekte', href: '/projects'},
+  {label: 'About', href: '/about'},
+  {label: 'Blog', href: 'https://medium.com/@vellip', target: '_blank'},
+]
 
-const Layout = ({children, onHideNav, onShowNav, showNav, siteTitle}) => (
-  <>
-    <Header siteTitle={siteTitle} onHideNav={onHideNav} onShowNav={onShowNav} showNav={showNav} />
-    <div className={styles.content}>{children}</div>
-    <footer className={styles.footer}>
-      <div className={styles.footerWrapper}>
-        <div className={styles.siteInfo}>
-          © {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a> &amp;
-          {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
-        </div>
+const Header = ({siteTitle}) => (
+  <header className={hStyles.header}>
+    <nav>
+      <ul className={hStyles.header__list}>
+        <li className={cn(hStyles.header__listItem, hStyles.header__listItemHome)}>
+          <Link to="/" className={hStyles.header__link} activeClassName={hStyles.header__linkActive}>
+            {siteTitle}
+          </Link>
+        </li>
+        {links.map((item, index) => (
+          <li className={hStyles.header__listItem}>
+            <Link
+              to={item.href}
+              className={hStyles.header__link}
+              target={item.target || '_self'}
+              activeClassName={hStyles.header__linkActive}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  </header>
+)
+
+const textMe = rnItem(['Schreib mir mal.', 'Noch Fragen?', 'Lass uns zusammenarbeiten!'])
+const callMe = rnItem(['Bisschen plaudern?', 'Lust zu quatschen?', 'Noch Fragen?'])
+
+const Footer = () => (
+  <footer className={fStyles.footer}>
+    <Container>
+      <div className={fStyles.footer__section}>
+        <h2 className={fStyles.footer__label}>{textMe}</h2>
+        <a href="mailto:mail@vellip.de?subject=Hallöchen" className={fStyles.footer__value}>
+          mail@vellip.de
+        </a>
       </div>
-    </footer>
+      <div className={fStyles.footer__section}>
+        <h2 className={fStyles.footer__label}>{callMe}</h2>
+        <p className={fStyles.footer__value}>+49 (0)170 38 72 190</p>
+      </div>
+      <div className={cn(fStyles.footer__section, fStyles.footer__sectionRight)}>
+        <nav>
+          <ul className={fStyles.footer__list}>
+            <li>
+              <a href="https://medium.com/@vellip" target="_blank">
+                <FontAwesomeIcon icon={faMediumM} aria-label="Medium Profil" />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/vellip_" target="_blank">
+                <FontAwesomeIcon icon={faTwitter} aria-label="Twitter Profil" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/philipp-veller-webdev/" target="_blank">
+                <FontAwesomeIcon icon={faLinkedinIn} aria-label="LinkedIn Profil" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/vellip" target="_blank">
+                <FontAwesomeIcon icon={faGithub} aria-label="Github Profil" />
+              </a>
+            </li>
+          </ul>
+          <Link to="/page/impressum-datenschutz">Impressum & Datenschutz</Link>
+        </nav>
+      </div>
+    </Container>
+  </footer>
+)
+
+const Layout = ({children, siteTitle}) => (
+  <>
+    <Header siteTitle={siteTitle} />
+    <main className={styles.content}>{children}</main>
+    <Footer />
   </>
 )
 
