47dbba8e8afb39274f8e09aa611561c35099ae61
Philipp Veller <mail@vellip.de>
2021-01-15 21:53:34 +0100

    rename sampleProject.js to project.js and add extra fields

--- studio/schemas/documents/project.js
+++ studio/schemas/documents/project.js
@@ -1,14 +1,18 @@
 import {format} from 'date-fns'
 
+
 export default {
-  name: 'sampleProject',
-  title: 'Sample project',
+  name: 'project',
+  title: 'Project',
   type: 'document',
+  fieldsets: [
+    {name: 'meta', title: 'Meta data'}
+  ],
   fields: [
     {
       name: 'title',
       title: 'Title',
-      type: 'string'
+      type: 'string',
     },
     {
       name: 'slug',
@@ -17,65 +21,85 @@ export default {
       description: 'Some frontend will require a slug to be set to be able to show the project',
       options: {
         source: 'title',
-        maxLength: 96
-      }
+        maxLength: 96,
+      },
     },
     {
       name: 'publishedAt',
       title: 'Published at',
       description: 'You can use this field to schedule projects where you show them',
-      type: 'datetime'
+      type: 'datetime',
     },
     {
       name: 'excerpt',
       title: 'Excerpt',
-      type: 'simplePortableText'
-    },
-    {
-      name: 'members',
-      title: 'Members',
-      type: 'array',
-      of: [{type: 'projectMember'}]
+      type: 'simplePortableText',
     },
     {
       name: 'startedAt',
       title: 'Started at',
-      type: 'datetime'
+      type: 'datetime',
+      fieldset: 'meta'
     },
     {
       name: 'endedAt',
       title: 'Ended at',
-      type: 'datetime'
+      type: 'datetime',
+      fieldset: 'meta'
     },
     {
       name: 'mainImage',
       title: 'Main image',
-      type: 'figure'
+      type: 'figure',
+    },
+    {
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{type: 'figure'}],
     },
     {
       name: 'categories',
       title: 'Categories',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}]
+      of: [{type: 'reference', to: {type: 'category'}}],
     },
     {
       name: 'body',
       title: 'Body',
-      type: 'projectPortableText'
+      type: 'projectPortableText',
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'url',
+      fieldset: 'meta'
+    },
+    {
+      name: 'agency',
+      title: 'Agency',
+      type: 'string',
+      fieldset: 'meta'
+    },
+    {
+      name: 'client',
+      title: 'Client',
+      type: 'string',
+      fieldset: 'meta'
     },
     {
       name: 'relatedProjects',
       title: 'Related projects',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'sampleProject'}}]
-    }
+      of: [{type: 'reference', to: {type: 'project'}}],
+    },
   ],
   preview: {
     select: {
       title: 'title',
       publishedAt: 'publishedAt',
       slug: 'slug',
-      media: 'mainImage'
+      media: 'mainImage',
     },
     prepare({title = 'No title', publishedAt, slug = {}, media}) {
       const dateSegment = format(publishedAt, 'YYYY/MM')
@@ -83,8 +107,8 @@ export default {
       return {
         title,
         media,
-        subtitle: publishedAt ? path : 'Missing publishing date'
+        subtitle: publishedAt ? path : 'Missing publishing date',
       }
-    }
-  }
+    },
+  },
 }
