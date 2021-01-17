547819df8181d523f3590ab95edc395e6870dc15
Philipp <VellerPhilipp@gmail.com>
2021-01-18 00:19:37 +0100

    Update gatsby-config.js
    fix gatsby-sitemap issue on deployment

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -9,6 +9,9 @@ const token = process.env.SANITY_READ_TOKEN
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.vellip.de`,
+  },
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-react-helmet',
